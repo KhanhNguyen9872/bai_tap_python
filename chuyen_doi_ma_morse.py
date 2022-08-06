b588f85452527cfb4c5e3b3d8a8f70cdd49c4562
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-08-06 09:31:38 +0700

    fix

--- chuyen_doi_ma_morse.py
+++ chuyen_doi_ma_morse.py
@@ -1,55 +1,51 @@
 import os
 
 def clear():
-    if (os.name == 'nt'):
-        os.system('cls')
-    else:
-        os.system('clear')
+	if (os.name == 'nt'):
+		os.system('cls')
+	else:
+		os.system('clear')
 
 error=1
-dichMorse={".-":"A","-...":"B","-.-.":"C","-..":"D",".":"E","..-.":"F","--.":"G","....":"H","..":"I",".---":"J","-.-":"K",".-..":"L","--":"M","-.":"N","---":"O",".--.":"P","--.-":"Q",".-.":"R","...":"S","-":"T","..-":"U","...-":"V",".--":"W","-..-":"X","-.--":"Y","--..":"Z",".--.-":"Â","-..-.":"Ê","---.":"Ô","-----":" "}
-chuyenKiTuSangMorse={"A":".-","B":"-...","C":"-.-.","D":"-..","E":".","F":"..-.","G":"--.","H":"....","I":"..","J":".---","K":"-.-","L":".-..","M":"--","N":"-.","O":"---","P":".--.","Q":"--.-","R":".-.","S":"...","T":"-","U":"..-","V":"...-","W":".--","X":"-..-","Y":"-.--","Z":"--..","Â":".--.-","Ê":"-..-.","Ô":"---."," ":"-----"}
+morse2text={".-":"A","-...":"B","-.-.":"C","-..":"D",".":"E","..-.":"F","--.":"G","....":"H","..":"I",".---":"J","-.-":"K",".-..":"L","--":"M","-.":"N","---":"O",".--.":"P","--.-":"Q",".-.":"R","...":"S","-":"T","..-":"U","...-":"V",".--":"W","-..-":"X","-.--":"Y","--..":"Z",".--.-":"Â","-..-.":"Ê","---.":"Ô","-----":" "}
+text2morse={v: k for k, v in morse2text.items()}
 
 if (__name__ == '__main__'):
-    while 1:
-        try:
-            clear()
-            chon = input('''
+	try:
+		while 1:
+			clear()
+			chon = input('''
  ===> TRÌNH DỊCH VÀ CHUYỂN ĐỔI MÃ MORSE <===
-    1. Dịch mã morse.
-    2. Chuyển sang mã morse.
-    3. Thoát.
-    ===================================
+	1. Dịch mã morse.
+	2. Chuyển sang mã morse.
+	3. Thoát.
+	===================================
 Nhập lựa chọn: ''')
-            if chon == '1':
-                print("\nNhập mã morse: ",end="")
-                list=[str(x) for x in input().split(" ")]
-                for i in list:
-                    try:
-                        print(dichMorse[i],end="")
-                    except:
-                        continue
-            elif chon == '2':
-                print("\nNhập văn bản cần chuyển sang morse: ",end="")
-                list=[char for char in input().upper()]
-                for i in list:
-                    try:
-                        print(chuyenKiTuSangMorse[i],end=" ")
-                    except:
-                        continue
-            elif chon == '3':
-                clear()
-                print("\nBạn đã thoát chương trình!")
-            else:
-                continue
-            print("\n")
-            error=0
-            exit()
-        except KeyboardInterrupt:
-            print("\n\n==============================\n\nBạn không thể thoát bằng Ctrl + C\n Vui lòng chọn Thoát trong menu để thoát!\n\n==============================\n\nBấm phím Enter để tiếp tục!\n")
-            input()
-            continue
-        except:
-            if (error == "1"):
-                print("\nChương trình gặp lỗi!\n Hãy liên hệ nhà phát triển!\n\n")
-            exit()
+			if chon == '1':
+				print("\nNhập mã morse: ",end="")
+				list=[str(x) for x in input().split(" ")]
+				for i in list:
+					try:
+						print(morse2text[i],end="")
+					except:
+						continue
+			elif chon == '2':
+				print("\nNhập văn bản cần chuyển sang morse: ",end="")
+				list=[char for char in input().upper()]
+				for i in list:
+					try:
+						print(text2morse[i],end=" ")
+					except:
+						continue
+			elif chon == '3':
+				clear()
+				print("\nBạn đã thoát chương trình!")
+			else:
+				continue
+			print("\n")
+			error=0
+			exit()
+	except:
+		if (error == "1"):
+			print("\nChương trình gặp lỗi!\n Hãy liên hệ nhà phát triển!\n\n")
+		exit()
