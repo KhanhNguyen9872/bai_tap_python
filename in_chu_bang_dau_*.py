421f3f20b981b73a2006929c34d8646f8f19f15c
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-08-20 16:47:44 +0000

    Add files via upload

--- in_chu_bang_dau_*.py
+++ in_chu_bang_dau_*.py
@@ -6,18 +6,40 @@
 #   *****  
 #  *       * 
 #  *       *
-## Ngang: 9
-## Dọc: 5
-count=0
-A=[5,13,15,21,22,23,24,25,29,35,37,45]
-user_input=str(input("Nhập 1 chữ [A-Z]: ").upper())
-for i in range (1,46,1):
-    if (i in (eval(user_input))):
-        print("*",end="")
-    else:
-        print(" ",end="")
-    count+=1
-    if (count == 9):
-        print("\n")
-        count=0
+A=[3,7,9,11,12,13,14,15,16,20,21,25,'5@','5@']
+B=[1, 2, 3, 4, 6, 10, 11, 12, 13, 14, 16, 20, 21, 22, 23, 24, '5@', '5@']
+C=[2, 3, 4, 5, 6, 11, 16, 22, 23, 24, 25, '5@', '6@']
+D=[1, 2, 3, 6, 10, 11, 15, 16, 20, 21, 22, 23, '5@', '5@']
+E=[1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 15, 16, 21, 22, 23, 24, 25, '5@', '5@']
+F=[1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 15, 16, 21, '5@', '5@']
+print("Nhập chữ [A-Z]: ",end="")
+user_input=[char for char in input().upper()]
+size=1
+portrait=0
+landscape=100
+for i in user_input:
+    try:
+        temp=str(eval(i)[-1])
+    except:
+        print(f"{i} not found!")
+        exit()
+    if (int(temp.replace('@','')) > portrait):
+	    portrait=int(temp.replace("@",""))
+    temp=str(eval(i)[-2])
+    if (int(temp.replace('@','')) < landscape):
+	    landscape=int(temp.replace('@',''))
+
+size_landscape=int(landscape)
+for i in range (1,portrait+1,1):
+    for char in user_input:
+        for i in range(size,size_landscape+1,1):
+            if (i in eval(char)):
+                print("*",end="")
+            else:
+                print(" ",end="")
+            if (char == user_input[-1]):
+                size+=1
+                size_landscape+=1
+        print(end="  ")
+    print(end="\n")
         
