a89da557483ec4b7eedd1f2d04a6d895df9ad8ad
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-08-21 04:11:51 +0000

    Add files via upload

--- Draw_*_thành_1_list.py
+++ Draw_*_thành_1_list.py
@@ -8,15 +8,27 @@ def clear():
         os.system("clear")
         
 def main():
-	global limit
-	print(f"Nhập dấu * [Độ dài: {limit}]\n Gõ [exit] để hoàn tất!\n Gõ [clear] để xóa!\n123456789")
+	global limit, num
+	print(f"Nhập dấu * [Độ dài: {limit}]\n Gõ [exit] để hoàn tất!\n Gõ [clear] để xóa!\n\n{num}")
 
-clear()
-limit=int(input("Nhập độ dài: "))
+answer={}
 user_input=""""""
 show=""""""
 num=""
+count=1
 lines=0
+
+clear()
+limit=int(input("Nhập độ dài: "))
+if (limit > 50) or (limit < 1):
+	print("Sai giới hạn [1-50]")
+	exit()
+for _ in range(0,limit,1):
+    num+=str(count)
+    count+=1
+    if (count==10):
+        count=0
+
 main()
 while True:
     inp=str(input())
@@ -25,6 +37,7 @@ while True:
     elif (inp == "clear"):
         user_input=""""""
         show=""""""
+        lines=0
         clear()
         main()
         print(show,end="")
@@ -38,12 +51,11 @@ while True:
         lines+=1
 
 count=1
-answer=[]
 for line in user_input:
-    if (line == "*"):
-        answer.append(count)
+    if (line != " "):
+        answer[count]=line
     count+=1
 
-answer.append(f"{limit}@")
-answer.append(f"{lines}@")
+answer["l"]=limit
+answer["p"]=lines
 print(f"\n Kết quả: \n{answer}")
